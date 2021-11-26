548ec5d3f45cb0ee02b88f7996b360d3559f9a92
dmd2222 <dmd20@gmx.de>
2021-11-26 07:37:48 +0100

    I did it https://github.com/dmd2222 on 26.11.2021 at  7:37:48,41

--- auto_update.php
+++ auto_update.php
@@ -108,7 +108,7 @@ if($use_own_gui) {
 
     echo(filemtime($write_output_file)+60);
     echo("******");
-    echo(date());
+    echo(time());
     if(filemtime($write_output_file)+60 < time()){
       CheckCommits($user,$repo,$do_update,$target_directory);
     }
